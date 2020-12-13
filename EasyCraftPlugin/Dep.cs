81af613ad0c8d8a24676fa19c1fec7e8440ce72e
Kengwang <1136772134@qq.com>
2020-12-13 12:03:50 +0800

    Initial Commit

---
+++ EasyCraftPlugin/Dep.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace EasyCraftPlugin
+{
+    public class Settings
+    {
+        public static Type PluginCallback;
+        public static string key;
+    }
+
+    class PluginCall
+    {
+        public static dynamic Call(string type, string data)
+        {
+
+            return Settings.PluginCallback.GetMethod("Handle").Invoke(null, new object[] { new PluginCallData
+            {
+                pluginid = Plugin.id,
+                key = Settings.key,
+                type = type,
+                data = data
+            } });
+        }
+    }
+
+    public struct PluginCallData
+    {
+        public string pluginid;
+        public string key;
+        public string type;
+        public string data;
+    }
+
+    public class FastConsole
+    {
+
+        public static void PrintInfo(string message)
+        {
+            PluginCall.Call("FastConsole.PrintInfo", message);
+        }
+
+        public static void PrintTrash(string message)
+        {
+            PluginCall.Call("FastConsole.PrintTrash", message);
+        }
+
+        public static void PrintSuccess(string message)
+        {
+            PluginCall.Call("FastConsole.PrintSuccess", message);
+        }
+
+        public static void PrintWarning(string message)
+        {
+            PluginCall.Call("FastConsole.PrintWarning", message);
+        }
+        public static void PrintError(string message)
+        {
+            PluginCall.Call("FastConsole.PrintError", message);
+        }
+        public static void PrintFatal(string message)
+        {
+            PluginCall.Call("FastConsole.PrintFatal", message);
+        }
+
+    }
+
+    public struct PluginInfo
+    {
+        public string id;
+        public string name;
+        public string author;
+        public string link;
+        public string description;
+    }
+
+
+}
---
+++ EasyCraftPlugin/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace EasyCraftPlugin
+{
+    public class Plugin
+    {
+        //下方修改成你的插件信息
+        public static string id = "top.easycraft.plugin.example";//插件ID 请尽量唯一
+        public static string name = "EasyCraft 测试插件";//插件名称
+        public static string author = "Kengwang @ EasyCraft Team";//作者名称
+        public static string description = "这是 EasyCraft 测试插件";//插件简介
+        public static string link = "https://www.easycraft.top";//插件地址
+        //上方修改成你的插件信息
+
+
+        /// <summary>
+        /// 当开始加载插件时调用,会返回插件信息,请不要阻塞此方法或改变返回格式
+        /// 请注意! 请不要再此方法下调用 EasyCraft API,所有在此方法下调用的操作都将被忽略
+        /// </summary>
+        /// <returns>插件信息</returns>
+        public static PluginInfo Initialize(Type PluginBack,string key)
+        {
+            Settings.PluginCallback = PluginBack;
+            Settings.key = key;
+            return new PluginInfo
+            {
+                id = id,
+                name = name,
+                author = author,
+                description = description,
+                link = link
+            };
+        }
+
+        /// <summary>
+        /// 当插件被启用时返回
+        /// </summary>
+        public static void OnEnable()
+        {
+            //我们建议您使用 FastConsole 指令,此指令会将输出内容定向到 EasyCraft 日志程序,从而使用户更好的反馈错误
+            FastConsole.PrintInfo("成功启用插件!");
+        }
+    }
+}
