8d70d66bf16570e8272098dd95ce42264951b792
kengwang <atkengwang@qq.com>
2021-08-13 14:33:10 +0800

    适配示例插件
    Fit for https://github.com/EasyCraftPanel/EasyCraft/commit/c608e4f9d7f059f78edff3d4b81ca6d1b4b285d7

--- EasyCraftPlugin/Dep.cs
+++ EasyCraftPlugin/Dep.cs
@@ -14,80 +14,38 @@ namespace EasyCraftPlugin
 
     class PluginCall
     {
-        public static dynamic Call(string type, Dictionary<string, string> data)
+        public static object Call(string type, object data)
         {
             return Settings.PluginCallback.GetMethod("Handle")
                 ?.Invoke(null, new object[]
                 {
-                    (object) new PluginCallData
+                    new Dictionary<string, object>()
                     {
-                        pluginid = Plugin.id,
-                        key = Settings.key,
-                        type = type,
-                        data = data
+                        { "id", Plugin.PluginInfo["id"] },
+                        { "key", Settings.key },
+                        { "func", type },
+                        { "data", data }
                     }
                 });
         }
     }
 
-    public struct PluginCallData
-    {
-        public string pluginid;
-        public string key;
-        public string type;
-        public Dictionary<string, string> data;
-    }
-
     class Server
     {
-        public bool isnull = true;
-
-        // Server Basic Info Start
-        public int Id;
-        public string Name;
-        public string Core;
-        public int Owner;
-        public bool Running;
-        public int Maxplayer;
-        public int Port;
-
-        public DateTime Expiretime;
-        // Server Basic Info End
-
-        public Server(int sid)
-        {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["sid"] = sid.ToString();
-            dynamic s = PluginCall.Call("Server.GetBasicInfo", a);
-            if (s is null)
-            {
-                isnull = true;
-                return;
-            }
-
-            isnull = false;
-            Core = s.Core;
-            Expiretime = s.Expiretime;
-            Id = s.Id;
-            Maxplayer = s.Maxplayer;
-            Name = s.Name;
-            Owner = s.Owner;
-            Port = s.Port;
-            Running = s.Running;
-        }
-
-        public bool SendCommand(string cmd)
+        public static bool SendCommand(string sid, string cmd)
         {
             Dictionary<string, string> args = new Dictionary<string, string>();
             args["cmd"] = cmd;
-            return PluginCall.Call("Server.SendCommand",args);
+            args["sid"] = sid;
+            return (bool)PluginCall.Call("Server.SendCommand", args);
         }
-        
-        public bool AddOutput(string output)
+
+        public static bool AddOutput(string sid, string output)
         {
             Dictionary<string, string> args = new Dictionary<string, string>();
             args["output"] = output;
-            return PluginCall.Call("Server.AddOutput",args);
+            args["sid"] = sid;
+            return (bool)PluginCall.Call("Server.AddOutput", args);
         }
     }
 
@@ -95,56 +53,27 @@ namespace EasyCraftPlugin
     {
         public static void PrintInfo(string message)
         {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["message"] = message;
-            PluginCall.Call("FastConsole.PrintInfo", a);
+            PluginCall.Call("FastConsole.PrintInfo", message);
         }
 
         public static void PrintTrash(string message)
         {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["message"] = message;
-            PluginCall.Call("FastConsole.PrintTrash", a);
+            PluginCall.Call("FastConsole.PrintTrash", message);
         }
-
-        public static void PrintSuccess(string message)
-        {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["message"] = message;
-            PluginCall.Call("FastConsole.PrintSuccess", a);
-        }
-
+        
         public static void PrintWarning(string message)
         {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["message"] = message;
-            PluginCall.Call("FastConsole.PrintWarning", a);
+            PluginCall.Call("FastConsole.PrintWarning", message);
         }
 
         public static void PrintError(string message)
         {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["message"] = message;
-            PluginCall.Call("FastConsole.PrintError", a);
+            PluginCall.Call("FastConsole.PrintError", message);
         }
 
         public static void PrintFatal(string message)
         {
-            Dictionary<string, string> a = new Dictionary<string, string>();
-            a["message"] = message;
-            PluginCall.Call("FastConsole.PrintFatal", a);
+            PluginCall.Call("FastConsole.PrintFatal", message);
         }
-    }
-
-    public struct PluginInfo
-    {
-        public string id;
-        public string name;
-        public string author;
-        public string link;
-        public string description;
-        public Dictionary<string,int> hooks;
-        public string[] auth;
-        public string version;
     }
 }
--- EasyCraftPlugin/Plugin.cs
+++ EasyCraftPlugin/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 
 namespace EasyCraftPlugin
@@ -8,28 +9,36 @@ namespace EasyCraftPlugin
     public class Plugin
     {
         //下方修改成你的插件信息
-        public static string id = "top.easycraft.plugin.example"; //插件ID 请尽量唯一
-        public static string name = "EasyCraft 测试插件"; //插件名称
-        public static string author = "Kengwang @ EasyCraft Team"; //作者名称
-        public static string description = "这是 EasyCraft 测试插件"; //插件简介
-        public static string version = "1.0.0"; //插件版本
-        public static string link = "https://www.easycraft.top"; //插件地址
+        public static Dictionary<string, string> PluginInfo = new()
+        {
+            { "id", "top.easycraft.plugin.example" }, //插件ID 请尽量唯一
+            { "name", "示例插件" }, //插件名称
+            { "version", "1.0.0" }, //插件版本
+            { "author", "Kengwang" }, //作者名称
+            { "description", "这是 EasyCraft 示例插件" }, //插件简介
+            { "link", "https://www.easycraft.top" } // 插件链接
+        };
         //上方修改成你的插件信息
 
         // 此处为你想要Hook的点位,如果不在此处填写将会无法收到相关事件
         private static Dictionary<string, int> Hooks = new Dictionary<string, int>()
         {
-            // {"Hook名称","优先级"}
-            // 优先级数字越大越先调用 ,请取 1 - 10 以内的数字,综合考虑,不要太极端,避免和其他插件造成冲突
-            {"ServerWillStart", 5}, //MC服务器准备开始运行 - 可拦截
-            {"ServerStarted", 5}, //MC服务器已经开始运行
-            {"ServerStop", 5} //MC服务器关闭
+            // {"Hook名称",优先级}
+            // 请保证方法名称和 Hook 名称一致
+            // 优先级数字越大越先调用 ,请取 1 - 10 以内的数字
+            // 综合考虑,不要太极端,避免和其他插件造成冲突
+            // 默认数字为 5 
+            { "OnEnable", 5 },
+            { "ServerWillStart", 5 }, //MC服务器准备开始运行 - 可拦截
+            { "ServerStarted", 5 }, //MC服务器已经开始运行
+            { "ServerStop", 5 } //MC服务器关闭
         };
         // 上方为你想要Hook的点位,如果不在此处填写将会无法收到相关事件
 
         //此处为你想要申请的权限,如果未在此处填写但是却尝试执行相关操作将会忽略
         private static string[] Auth =
         {
+            "FastConsole.PrintInfo", //控制台输出信息权限
             "StartServer", //服务器开机权限
             "StopServer", //服务器关机权限
             "ServerBasicInfo", //服务器基本信息
@@ -37,24 +46,20 @@ namespace EasyCraftPlugin
         //上方为你想要申请的权限,如果未在此处填写但是却尝试执行相关操作将会忽略
 
         /// <summary>
+        /// !!! 请不要修改此方法 !!!
         /// 当开始加载插件时调用,会返回插件信息,请不要阻塞此方法或改变返回格式
         /// 请注意! 请不要再此方法下调用 EasyCraft API,所有在此方法下调用的操作都将被忽略
         /// </summary>
         /// <returns>插件信息</returns>
-        public static object Initialize(Type PluginBack, string key)
+        public static object OnLoad(Type PluginBack, string key)
         {
             Settings.PluginCallback = PluginBack;
             Settings.key = key;
-            return (object) new PluginInfo
+            return new Dictionary<string, Dictionary<string, string>>()
             {
-                id = id,
-                name = name,
-                author = author,
-                description = description,
-                link = link,
-                version = version,
-                hooks = Hooks,
-                auth = Auth
+                { "PluginInfo", PluginInfo },
+                { "Hooks", Hooks.ToDictionary(t => t.Key, t => t.Value.ToString()) },
+                { "Request", Auth.ToDictionary(t => t, t => "true") }
             };
         }
 
@@ -64,24 +69,17 @@ namespace EasyCraftPlugin
         public static void OnEnable()
         {
             //我们建议您使用 FastConsole 指令,此指令会将输出内容定向到 EasyCraft 日志程序,从而使用户更好的反馈错误
-            FastConsole.PrintInfo("成功启用插件!");
+            FastConsole.PrintInfo("EasyCraft Example Plugin 被启用!");
         }
 
         /// <summary>
         /// 服务器将要开启
         /// </summary>
-        public static bool ServerWillStart(int sid, Process p)
+        public static bool ServerWillStart(Dictionary<string, object> server)
         {
-            FastConsole.PrintInfo("监听到服务器开启事件: " + sid + " Path:" + p.StartInfo.FileName);
+            FastConsole.PrintInfo("监听到服务器开启事件: " + server["id"]);
             //你在此处可以修改Process相关配置
             //你也可以通过sid获取服务器的信息
-            Server s = new Server(sid);
-            if (!s.isnull)
-            {
-                //请务必检查一下,防止报错发生
-                FastConsole.PrintInfo("获取到了服务器名称: " + s.Name);
-            }
-
             return true;
         }
 
